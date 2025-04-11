464d2a759cca07d3bfe34aad8b7687198b5c2254
farrux-torayev <farruxtorayev590@gmail.com>
2025-04-11 16:16:16 +0500

    sw

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Footer from "./components/footer";
 import CategoriesMain from "./components/main";
 import MainMapping from "./components/MainMapping";
 import Navbar from "./components/navbar";
+import Hero from "./components/Swiper";
 
 function App() {
   return (
     <div className=" m-auto w-[1200px]">
       <Navbar />
+      <Hero />
       <div className="flex">
         <CategoriesMain />
         <MainMapping />
